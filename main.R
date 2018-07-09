a8e45c59eb024037583c2cf24f39a88630fc6779
Jesse Bilton <hakefu@Jesses-MacBook-Pro.local>
2018-07-09 13:52:07 -0400

    Main from Mac

---
+++ main.R
@@ -0,0 +1,27 @@
+# Audience-AI
+#
+# Displays notes and chords contained in a .wav audio file.
+#
+# Written by Jesse Bilton and Evangeline Joron-Parrot, 2013-2018
+
+# Load Packages
+
+
+# Init variables
+
+
+# Load Audio File
+
+
+# Sample analysis
+
+# 	Perform FFT
+
+
+# 	Extract peaks
+
+
+# 	Matchmaking
+
+
+#	
