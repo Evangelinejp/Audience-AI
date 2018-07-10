1380421bdab23831f9c6b6f94c1f49744d929f0b
Jesse Bilton <jesse.bilton@gmail.com>
2018-07-10 18:51:34 -0400

    pre-AI workshop

---
+++ load_audio_file.R
@@ -0,0 +1,10 @@
+# load_audio_file.R
+#
+# Loads 'load audio file' dialog routine.
+#
+# Written by Jesse Bilton, 2018
+
+enter_filename <- function() {
+	filename <- readline("\nEnter audio file name for music data extraction: ")	
+}
+
